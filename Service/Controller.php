96f0a398cae89f38a7e78009e582b26b884413f5
Jarzon <j@masterj.net>
2017-06-13 17:31:39 -0400

    Update to Prim 0.5.*

--- Service/Controller.php
+++ Service/Controller.php
@@ -1,14 +1,16 @@
 <?php
 namespace UserPack\Service;
 
+// TODO: Make it a User Class Service and add it using a container Trait
+
 trait Controller
 {
     public $logged = false;
     public $user_id = 0;
 
-    function __construct($view)
+    function __construct($view, $container)
     {
-        parent::__construct($view);
+        parent::__construct($view, $container);
 
         if(session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
--- config/routing.php
+++ config/routing.php
@@ -1,13 +1,13 @@
 <?php
-$this->addRoute(['GET', 'POST'], '/signup', 'UserPack\User', 'signup');
-$this->addRoute(['GET', 'POST'], '/signin', 'UserPack\User', 'signin');
-$this->addRoute(['GET', 'POST'], '/signout', 'UserPack\User', 'signout');
+$this->both('/signup', 'UserPack\User', 'signup');
+$this->both('/signin', 'UserPack\User', 'signin');
+$this->both('/signout', 'UserPack\User', 'signout');
 
 // TODO: implement admin
 $this->addGroup('/admin', function($r) {
-    $r->addRoute(['GET', 'POST'], '[/{page:[\d+]?}]', 'UserPack\User', 'list');
+    $r->both('[/{page:[\d+]?}]', 'UserPack\User', 'list');
 
-    $r->addRoute(['GET', 'POST'], '/edit/{user:[\d+]?}', 'UserPack\User', 'showUser');
+    $r->both('/edit/{user:[\d+]?}', 'UserPack\User', 'showUser');
 
     $r->get('/delete/{user:[\d+]?}', 'UserPack\User', 'deleteUser');
 });
