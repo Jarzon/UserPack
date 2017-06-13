a9054cde4b84f8c8ddd7b18ff151ebc0f5db21c5
Jarzon <j@masterj.net>
2017-06-13 12:38:17 -0400

    Update routing config for Prim\Router

--- config/routing.php
+++ config/routing.php
@@ -1,13 +1,13 @@
 <?php
-$this->router->addRoute(['GET', 'POST'], '/signup', ['\UserPack\Controller\User', 'signup']);
-$this->router->addRoute(['GET', 'POST'], '/signin', ['\UserPack\Controller\User', 'signin']);
-$this->router->addRoute(['GET', 'POST'], '/signout', ['\UserPack\Controller\User', 'signout']);
+$this->addRoute(['GET', 'POST'], '/signup', 'UserPack\User', 'signup');
+$this->addRoute(['GET', 'POST'], '/signin', 'UserPack\User', 'signin');
+$this->addRoute(['GET', 'POST'], '/signout', 'UserPack\User', 'signout');
 
-// TODO:
-$this->router->addGroup('/admin', function($r) {
-    $r->addRoute(['GET', 'POST'], '[/{page:[\d+]?}]', ['\UserPack\Controller\User', 'list']);
+// TODO: implement admin
+$this->addGroup('/admin', function($r) {
+    $r->addRoute(['GET', 'POST'], '[/{page:[\d+]?}]', 'UserPack\User', 'list');
 
-    $r->addRoute(['GET', 'POST'], '/edit/{user:[\d+]?}', ['\UserPack\Controller\User', 'showUser']);
+    $r->addRoute(['GET', 'POST'], '/edit/{user:[\d+]?}', 'UserPack\User', 'showUser');
 
-    $r->get('/delete/{user:[\d+]?}', ['\UserPack\Controller\User', 'deleteUser']);
+    $r->get('/delete/{user:[\d+]?}', 'UserPack\User', 'deleteUser');
 });
