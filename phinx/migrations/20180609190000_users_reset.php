f84ba08e9e6352a81aa08131c8df71e249e3a82f
Jarzon <j@jasonvaillancourt.ca>
2018-06-09 19:45:52 -0400

    Add reset field migration

---
+++ phinx/migrations/20180609190000_users_reset.php
@@ -0,0 +1,14 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class UsersReset extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('users');
+        $table
+            ->addColumn('reset', 'string', ['limit' => 40, 'after' => 'status'])
+            ->create();
+    }
+}
