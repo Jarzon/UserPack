cdbe093fa97579f825a5d3ebceff2e64019c2bf5
Jarzon <j@jasonvaillancourt.ca>
2019-04-30 22:17:36 -0400

    Fix Admin contructor signature

--- Controller/Admin.php
+++ Controller/Admin.php
@@ -1,15 +1,16 @@
 <?php
 namespace UserPack\Controller;
 
+use Jarzon\Localization;
 use UserPack\Model\UserModel;
 
 class Admin extends User
 {
     protected $admin;
 
-    public function __construct(\Prim\View $view, array $options, \UserPack\Service\User $user, UserModel $userModel, \PrimPack\Service\Admin $admin)
+    public function __construct(\Prim\View $view, array $options, \UserPack\Service\User $user, Localization $local, UserModel $userModel, \PrimPack\Service\Admin $admin)
     {
-        parent::__construct($view, $options, $user, $userModel);
+        parent::__construct($view, $options, $user, $local, $userModel);
 
 
         $this->admin = $admin;
