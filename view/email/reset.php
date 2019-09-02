55f9d5f51e666f9723d09ce7d0d20d79a4d54926
Jarzon <j@jasonvaillancourt.ca>
2019-09-01 23:43:25 -0400

    Add new line in reset email

--- view/email/reset.php
+++ view/email/reset.php
@@ -1,4 +1,5 @@
 Hello <?=$user->name?>,
 
 We have receive a request to reset your password. You can do so by going at this address: https://www.libellum.ca/reset/<?=$user->email?>/<?=$reset?>
+
 If you didn't request to reset your password, you can skip this message and your password is not going to be changed.
