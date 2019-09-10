e8f5e5e0c432fd446d0fa28016d8ac2b20dcc1e6
Jarzon <j@jasonvaillancourt.ca>
2019-09-10 18:19:13 -0400

    Fix users admin page number not being optional

--- config/routing_admin.php
+++ config/routing_admin.php
@@ -1,6 +1,6 @@
 <?php
 $this->addGroup('/admin/users/', function($r) {
-    $r->both('{user:\d+}', 'UserPack\Admin', 'list');
+    $r->both('{user:\d*}', 'UserPack\Admin', 'list');
 
     $r->both('edit/{user:\d+}', 'UserPack\Admin', 'show');
 });
