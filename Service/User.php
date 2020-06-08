f79e857e5114433f5e0f760226c1e2cf680809ce
Jarzon <j@jasonvaillancourt.ca>
2020-06-08 12:19:08 -0400

    Create init function

--- Service/User.php
+++ Service/User.php
@@ -22,7 +22,9 @@ class User
                 ]
             ]
         ];
+    }
 
+    public function init() {
         if(session_status() !== PHP_SESSION_ACTIVE && !headers_sent()) {
             session_start();
         }
